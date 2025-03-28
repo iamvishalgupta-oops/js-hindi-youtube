7c168eb2658f2ed835287d78cf367f85c150f7f6
Vishal Gupta <132651145+iamvishalgupta-oops@users.noreply.github.com>
2025-03-28 17:22:22 +0000

    variable done

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,7 @@
-console.log("Vishal")
+const nameId=1901;
+let userName="Vishal"
+var userPass="okpo"
+userAddress="Lucknow"
+let userScreen;
+
+console.table([nameId,userName,userPass,userAddress,userScreen,])
